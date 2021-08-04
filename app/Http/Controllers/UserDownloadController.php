27524f96657fc5ddc02565b46a32f22ac5ebf9f5
jeromesoriano <sorianojerome@hotmail.com>
2021-08-05 02:40:12 +0800

    Initial App

---
+++ app/Http/Controllers/UserDownloadController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserDownload;
+use Illuminate\Http\Request;
+
+class UserDownloadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\UserDownload  $userDownload
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserDownload $userDownload)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\UserDownload  $userDownload
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserDownload $userDownload)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\UserDownload  $userDownload
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserDownload $userDownload)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\UserDownload  $userDownload
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserDownload $userDownload)
+    {
+        //
+    }
+}
